c7d8fa953b4c4f1c9a1f71da13c1e77100ec3497
THEosusi <bp20129@shibaura-it.ac.jp>
2025-09-20 00:49:51 +0200

    add variable for taking PR branch

--- infra/variables.tf
+++ infra/variables.tf
@@ -12,3 +12,8 @@ variable "instance_type" {
   type    = string
   default = "t3.micro"
 }
+
+variable "pr_number" {
+  type        = string
+  description = "GitHub Pull Request number for ephemeral environment"
+}
